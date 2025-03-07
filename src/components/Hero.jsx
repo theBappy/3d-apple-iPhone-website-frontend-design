9463852204fad92a93e185a2d7d0293dcbe3ba3b
theBappy <abappy575@gmail.com>
2025-03-07 17:46:44 +0600

    done

---
+++ src/components/Hero.jsx
@@ -0,0 +1,58 @@
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { heroVideo, smallHeroVideo } from '../utils'
+import { useState } from 'react'
+import { useEffect } from 'react'
+
+const Hero = () => {
+
+    const [videoSrc, setVideoSrc] = useState(window.innerWidth < 760 ? smallHeroVideo : heroVideo)
+
+    const handleVideoSrc = () =>{
+        if(window.innerWidth< 760){
+            setVideoSrc(smallHeroVideo)
+        }else{
+            setVideoSrc(heroVideo)
+        }
+    }
+    useGSAP(() =>{
+        gsap.to("#hero", {
+            opacity: 1,
+            delay: 2,
+        })
+        gsap.to('#cta', {
+            opacity: 1,
+            y: -50,
+            delay: 2,
+        })
+    }, [])
+
+
+    useEffect(() =>{
+       window.addEventListener('resize', handleVideoSrc)
+       return () => {
+        window.removeEventListener('resize', handleVideoSrc)
+       } 
+    }, [])
+
+
+  return (
+    <section className="w-full nav-height bg-black relative">
+        <div className="h-5/6 w-full flex-col flex-center">
+            <p id='hero' className="hero-title">iPhone 15 Pro</p>
+            <div className='md:w-10/12 w-9/12'>
+                <video className='pointer-events-none' autoPlay muted playsInline={true} key={videoSrc}>
+                    <source src={videoSrc} type='video/mp4' />
+                </video>
+            </div>
+        </div>
+
+        <div className="flex mt-5 flex-col items-center opacity-0 translate-y-20" id='cta'>
+            <a href="#highlights" className='btn'>Buy</a>
+            <p className='font-normal text-xl'>From $199/month or $999</p>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ src/main.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://51096f9838ae1d1d9b38fa89e262adcb@o4508935353794560.ingest.us.sentry.io/4508935358513152",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration({
+      maskAllText: false,
+      blockAllMedia: false,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
